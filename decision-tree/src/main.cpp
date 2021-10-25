9806897371fa0323891d1c8891d53c243d3eb2ce
RenzoT <renzo.tenazoa@utec.edu.pe>
2021-10-24 23:28:12 -0500

    kfol

--- decision-tree/src/main.cpp
+++ decision-tree/src/main.cpp
@@ -2,6 +2,18 @@
 #include "DecisionTree.h"
 #include "CSVReader.h"
 
+float accuracy(vector<float> a, vector<float> b){
+	int countError = 0;
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] != b[i]) {
+			countError += 1;
+			//cout << "Error" << endl;
+		}
+	}
+
+	return 100 - (float)(countError*100) / b.size();
+}
+
 int main(int argc, char *argv[]) {
 	/*
 	auto features = CSVReader<Iris>::read("iris.csv");
@@ -12,21 +24,18 @@ int main(int argc, char *argv[]) {
 	*/
 
 	auto decisionTree = DecisionTree("gender_classification.csv",3,5);
-	decisionTree.print();
+	//decisionTree.print();
 	auto test =  decisionTree.testingData();
 	auto predict = decisionTree.predict(test);
-
 	auto real_results = decisionTree.getRealResults(test);
 
-	int countError = 0;
-	for (int i = 0; i < test.size(); i++) {
-		if (predict[i] != real_results[i]) {
-			countError += 1;
-			//cout << "Error" << endl;
-		}
+	auto errores = decisionTree.KFoldError(2);
+
+	for(auto it:errores){
+		cout << it << endl;
 	}
-	cout << "Hay " << countError << " errores" << endl;
-	cout <<"Accuracy: " << 100 - (float)(countError*100) / real_results.size() << endl;
+
+	cout <<"Accuracy: " << accuracy(predict, real_results) << endl;
 	//decisionTree.generatePDF();
 	return 0;
 }
